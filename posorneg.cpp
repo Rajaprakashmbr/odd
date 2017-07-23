92e2a7bfb28d4907603b594c54e5459049564ded
Rajaprakashmbr <rajaprakashmbr1996@gmail.com>
2017-07-23 10:14:03 +0530

    Create posorneg.cpp

---
+++ posorneg.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter the number:\n";
+cin<<n;
+if(n>0)
+cout<<"the number"<<n<<"ispositive";
+else if(n<0)
+cout<<"the number"<<n<<"is negative";
+else
+cout<<"the number"<<n<<"not positive and not nagative";
+return 0;
+}
